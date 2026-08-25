af40fff838ce89373eea6e4619b441542691e431
gabrielarochaf <oliveirgabriela278@gmail.com>
2026-08-25 15:21:47 -0300

    Controller status delivery

--- src/routes/deliveries-routes.ts
+++ src/routes/deliveries-routes.ts
@@ -3,13 +3,17 @@ import { Router } from "express";
 import { DeliveriesController } from "../controllers/deliveries-controller";
 import { ensureAuthenticated } from "../middlewares/ensure-autheticated";
 import { verifyUserAuthorization } from "../middlewares/verify-user-authorized";
+import { DeliveriesStatusController } from "../controllers/deliveries-status-controller";
+
 
 const deliveriesRoutes = Router();
 const deliveriesController = new DeliveriesController();
+const deliveriesStatusController = new DeliveriesStatusController();
 
 deliveriesRoutes.use(ensureAuthenticated, verifyUserAuthorization(["sale"]));
 
 deliveriesRoutes.post("/", deliveriesController.create);
 deliveriesRoutes.get("/", deliveriesController.index);
+deliveriesRoutes.patch("/:id/status", deliveriesStatusController.update);
 
 export { deliveriesRoutes };
