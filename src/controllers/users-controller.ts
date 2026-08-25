b410f5baf1b728837057ca81b1a29c2e7e0fc298
gabrielarochaf <oliveirgabriela278@gmail.com>
2026-08-24 22:37:17 -0300

    Add data to database, user

--- src/controllers/users-controller.ts
+++ src/controllers/users-controller.ts
@@ -1,16 +1,37 @@
 import { Request, Response } from "express";
 import { z } from "zod";
+import { hash } from "bcrypt";
+import { prisma } from "../database/prisma";
+import { AppError } from "../utils/AppError";
 
 class UsersController {
-  create(request: Request, response: Response) {
+  async create(request: Request, response: Response) {
     const bodySchema = z.object({
-      name: z.string().min(1),
+      name: z.string().min(3),
       email: z.string().email(),
       password: z.string().min(6),
     });
 
     const { name, email, password } = bodySchema.parse(request.body);
-    return response.json({ message: "Ok" });
+
+    const userWithSameEmail = await prisma.user.findFirst({ where: { email } });
+
+    if (userWithSameEmail) {
+      throw new AppError("Email already exists", 409);
+    }
+    const hashedPassword = await hash(password, 8);
+
+    const user = await prisma.user.create({
+      data: {
+        name,
+        email,
+        password: hashedPassword,
+      },
+    });
+
+    const { password: _, ...userWithoutPassword } = user;
+
+    return response.status(201).json(userWithoutPassword);
   }
 }
 
