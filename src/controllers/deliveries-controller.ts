7c14ad11f0825abae08aca6d67d06d00ce3a6d5e
gabrielarochaf <oliveirgabriela278@gmail.com>
2026-08-25 15:03:19 -0300

    List deliveries

--- src/controllers/deliveries-controller.ts
+++ src/controllers/deliveries-controller.ts
@@ -1,9 +1,32 @@
 import { Request, Response } from "express";
+import { prisma } from "@/database/prisma";
+import { z } from "zod";
 
 class DeliveriesController {
-    create(request: Request, response: Response) {
-        return response.status(201).json({ message: "Delivery created successfully" });
-    }
+  async create(request: Request, response: Response) {
+    const bodySchema = z.object({
+      user_id: z.string().uuid(),
+      description: z.string(),
+    });
+
+    const { user_id, description } = bodySchema.parse(request.body);
+
+    await prisma.delivery.create({
+      data: {
+        userId: user_id,
+        description,
+      },
+    });
+
+    return response
+      .status(201)
+      .json({ message: "Delivery created successfully" });
+  }
+
+  async index(request: Request, response: Response) {
+    const deliveries = await prisma.delivery.findMany();
+    return response.status(200).json(deliveries);
+  }
 }
 
-export {DeliveriesController}
+export { DeliveriesController };
--- src/routes/deliveries-routes.ts
+++ src/routes/deliveries-routes.ts
@@ -1,15 +1,15 @@
-import {Router} from "express";
+import { Router } from "express";
 
-import {DeliveriesController} from "../controllers/deliveries-controller";
-import {ensureAuthenticated} from "../middlewares/ensure-autheticated";
-import {verifyUserAuthorization} from "../middlewares/verify-user-authorized";
-
+import { DeliveriesController } from "../controllers/deliveries-controller";
+import { ensureAuthenticated } from "../middlewares/ensure-autheticated";
+import { verifyUserAuthorization } from "../middlewares/verify-user-authorized";
 
 const deliveriesRoutes = Router();
 const deliveriesController = new DeliveriesController();
 
 deliveriesRoutes.use(ensureAuthenticated, verifyUserAuthorization(["sale"]));
 
-deliveriesRoutes.post("/", deliveriesController.create)
+deliveriesRoutes.post("/", deliveriesController.create);
+deliveriesRoutes.get("/", deliveriesController.index);
 
-export {deliveriesRoutes}
+export { deliveriesRoutes };
