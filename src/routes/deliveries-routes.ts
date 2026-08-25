ed460eb76621339c371e19681b85dc92bc30d9c3
gabrielarochaf <oliveirgabriela278@gmail.com>
2026-08-25 14:50:25 -0300

    verify user authentication

--- src/routes/deliveries-routes.ts
+++ src/routes/deliveries-routes.ts
@@ -2,11 +2,14 @@ import {Router} from "express";
 
 import {DeliveriesController} from "../controllers/deliveries-controller";
 import {ensureAuthenticated} from "../middlewares/ensure-autheticated";
+import {verifyUserAuthorization} from "../middlewares/verify-user-authorized";
+
 
 const deliveriesRoutes = Router();
 const deliveriesController = new DeliveriesController();
 
-deliveriesRoutes.use(ensureAuthenticated);
+deliveriesRoutes.use(ensureAuthenticated, verifyUserAuthorization(["sale"]));
+
 deliveriesRoutes.post("/", deliveriesController.create)
 
 export {deliveriesRoutes}
