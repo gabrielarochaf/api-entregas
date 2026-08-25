9c47cb558ae8cfda1ba78821c088a5befad4b562
gabrielarochaf <oliveirgabriela278@gmail.com>
2026-08-25 14:34:29 -0300

    export ensure authenticated

--- src/middlewares/ensure-autheticated.ts
+++ src/middlewares/ensure-autheticated.ts
@@ -13,17 +13,22 @@ function ensureAuthenticated(
   response: Response,
   next: NextFunction,
 ) {
-    try {
-        const authHeader = request.headers.authorization;
-        if(!authHeader) {
-            throw new AppError("JWT token is missing", 401);
-        }
-        const [, token] = authHeader.split(" ");
-
-        const {role, sub: user_id} = verify(token, authConfig.jwt.secret) as TokenPayload;
-        request.user = { id: user_id, role };
-        return next();
-    } catch (error) {
-        throw new AppError("Invalid JWT token", 401);
+  try {
+    const authHeader = request.headers.authorization;
+    if (!authHeader) {
+      throw new AppError("JWT token is missing", 401);
     }
+    const [, token] = authHeader.split(" ");
+
+    const { role, sub: user_id } = verify(
+      token,
+      authConfig.jwt.secret,
+    ) as TokenPayload;
+    request.user = { id: user_id, role };
+    return next();
+  } catch (error) {
+    throw new AppError("Invalid JWT token", 401);
+  }
 }
+
+export { ensureAuthenticated };
